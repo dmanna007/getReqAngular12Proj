da58e094afe7fd7683066ea0b80d7371f161a527
dmanna007 <d.manna007@gmail.com>
2021-12-11 15:29:51 +0530

    Implement the get method and subscribe the responce and render with bootstrap table & console log

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-user',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  User: any;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    let responce = this.http.get("https://jsonplaceholder.typicode.com/users");
+    responce.subscribe(data => console.log(data));
+    responce.subscribe((data) => this.User = data);
   }
-
 }
